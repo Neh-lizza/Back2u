ca6fe55d9b0aec6e20c07d00084155d5042cdc12
TekNeh <nehhlizza@gmail.com>
2026-02-20 15:51:57 +0100

    Create vite.config.js for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Configure options here, e.g., base, plugins, etc.
+  root: './src',
+  build: {
+    outDir: '../dist',
+  },
+});
